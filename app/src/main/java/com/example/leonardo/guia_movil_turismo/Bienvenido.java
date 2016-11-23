7398f8d9bfe9fe533048956100123385d65e5eea
Ivan <ivansanchez_1988@hotmail.com>
2016-11-22 22:30:39 -0500

    Bug Fix: El codigo de Byron solo dejaba loguear via Facebook. Se corrigio temporalmente, necesita ser arreglado de manera permanente

--- app/src/main/java/com/example/leonardo/guia_movil_turismo/Bienvenido.java
+++ app/src/main/java/com/example/leonardo/guia_movil_turismo/Bienvenido.java
@@ -1,11 +1,11 @@
 package com.example.leonardo.guia_movil_turismo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 import com.facebook.login.LoginManager;
-import com.facebook.AccessToken;
 
 public class Bienvenido extends AppCompatActivity {
 
@@ -13,8 +13,17 @@ public class Bienvenido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenido);
-        if(AccessToken.getCurrentAccessToken() == null){
+
+        //TODO Corregir este codigo de Byron porque solo permite el login via facebook, no el login del app
+        //Si el usuario se logea via el metodo del app, el codigo de abajo lo regresa al login y eso es un error
+        /*if(AccessToken.getCurrentAccessToken() == null){
             loginActivity();
+        }*/
+        try {
+            Thread.sleep(1000);
+            ir_recurso();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -31,7 +40,7 @@ public class Bienvenido extends AppCompatActivity {
         finish();
     }
 
-    public void ir_recurso(View view){
+    public void ir_recurso(){
         Intent i=new Intent(this,Recurso.class);
         startActivity(i);
     }
